226ab5766f22cef132389515638e07719ea1ca1e
Dilip965 <nandiwaledilip@gmail.com>
2024-08-23 14:02:32 +0530

    everything is uploaded

---
+++ chaiaurdjango/chaiaurdjango/views.py
@@ -0,0 +1,15 @@
+from django.http import HttpResponse
+from django.shortcuts import render
+
+def home(request):
+    return HttpResponse("<h1>Welcome to Chai's Django Project: Home page</h1>")
+
+def about(request):
+    return HttpResponse("<h1>Welcome to Chai's Django Project: About page</h1>")
+
+def contact(request):
+    return HttpResponse("<h1>Welcome to Chai's Django Project: Contact page</h1>")
+
+
+def dilip(request):
+    return render(request, 'index.html')
